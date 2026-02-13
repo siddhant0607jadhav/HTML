1c53d1afb492f52ca8441433a133b02d13ffab64
siddhant0607jadhav <jadhavsiddhant54@gmail.com>
2026-02-13 15:33:53 +0530

    new js file

---
+++ js1.js
@@ -0,0 +1,22 @@
+/* let a;
+let b;
+a=40;
+b=30;
+
+ for comment use Alt shift a
+console.log("hello  Addition is =", a+b);
+console.log("hello  sub is =", a-b);
+console.log("hello  multiplication is =", a*b);
+console.log("hello  divison is =", a/b);
+console.log("power",a**a); */
+
+let marks=99;
+if(marks>=75){
+    console.log("distriction");
+}else if(marks>=30){
+    console.log("First class");
+
+}else{
+    console.log("fail");
+}
+
